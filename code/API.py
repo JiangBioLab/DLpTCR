15e4e0a12dcbe180fbb68a3e9e6e014ad08e2796
JiangBioLab <72069543+JiangBioLab@users.noreply.github.com>
2024-04-22 14:42:02 +0800

    Update API.py

--- code/API.py
+++ code/API.py
@@ -30,4 +30,4 @@ if not user_dir_Exists:
     os.makedirs(user_dir)
     
 error_info,TCRA_cdr3,TCRB_cdr3,Epitope = deal_file(input_file_path, user_dir, model_select)
-output_file_path = save_outputfile(user_dir, user_select, input_file_path,TCRA_cdr3,TCRB_cdr3,Epitope)
+output_file_path = save_outputfile(user_dir, model_select , input_file_path,TCRA_cdr3,TCRB_cdr3,Epitope)
